ac31e7f0b53e22dab632e21fef6b3a5727c9d508
Dennis Cual <denniscual0716@gmail.com>
2023-08-03 21:42:47 +0800

    use useNavigate to update the search params and better history stack

--- ui/src/routes/root/page.tsx
+++ ui/src/routes/root/page.tsx
@@ -1,4 +1,4 @@
-import { LoaderFunction, useLoaderData } from 'react-router-dom';
+import { LoaderFunction, useLoaderData, useNavigate } from 'react-router-dom';
 import {
 	Button,
 	FormControl,
@@ -12,11 +12,21 @@ import {
 import ClientTable from './ClientTable';
 import { getClients } from 'services/api';
 import { Search } from '@mui/icons-material';
+import { ElementRef, useEffect, useRef } from 'react';
 
 export default function Root() {
 	const { clients, q } = useLoaderData() as LoaderData;
+	const navigate = useNavigate();
+	const searchRef = useRef<ElementRef<'input'>>(null);
 
 	console.log({ clients, q });
+
+	// Synchronize input value with the URL Search Params
+	useEffect(() => {
+		if (searchRef.current) {
+			searchRef.current.value = q;
+		}
+	}, [q]);
 
 	return (
 		<Stack gap={4}>
@@ -28,16 +38,27 @@ export default function Root() {
 					<FormControl sx={{ width: '35ch' }} variant='outlined'>
 						<InputLabel htmlFor='search-clients'>Search clients...</InputLabel>
 						<OutlinedInput
-							id='search-clients'
-							type='text'
+							ref={searchRef}
+							type='search'
 							endAdornment={
 								<InputAdornment position='end'>
 									<Search />
 								</InputAdornment>
 							}
 							label='Search clients...'
+							defaultValue={q}
+							onChange={(event) => {
+								// Managing history stack
+								const isFirstSearch = q === '';
+								// Create new stack if the query is empty. Else, update the search params while replacing the current stack.
+								// In this way, we can avoid polluting the history stack.
+								navigate(`/?${new URLSearchParams({ q: event.currentTarget.value })}`, {
+									replace: !isFirstSearch,
+								});
+							}}
 						/>
 					</FormControl>
+
 					<Button size='small' variant='contained'>
 						Create New Client
 					</Button>
@@ -52,6 +73,9 @@ export default function Root() {
 
 type LoaderData = { clients: IClient[]; q: string };
 
+/**
+ * Use caching mechanism here to avoid sending unnecessary requests to the server.
+ */
 export const loader: LoaderFunction = async ({ request }) => {
 	const clients = await getClients();
 	const url = new URL(request.url);
