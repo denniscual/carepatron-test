c8d7e24d1513351e6070c654d6838331c1c48ed0
Dennis Cual <denniscual0716@gmail.com>
2023-08-03 17:17:29 +0800

    ignore warning from a test

--- ui/src/App.test.tsx
+++ ui/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+	render(<App />);
+	const linkElement = screen.getByText(/learn react/i);
+	// @ts-expect-error @testing-library/jest-dom is not loaded properly
+	expect(linkElement).toBeInTheDocument();
 });
