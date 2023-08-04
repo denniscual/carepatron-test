2c3b2da288c36a763ec48c72dc912c683ca9165f
Dennis Cual <denniscual0716@gmail.com>
2023-08-04 13:28:42 +0800

    add No client row and remove old implementation

--- ui/src/routes/root/ClientTable.tsx
+++ ui/src/routes/root/ClientTable.tsx
@@ -5,43 +5,12 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import ClientRow from './ClientRow';
-
 import { Fragment, forwardRef } from 'react';
 import { TableVirtuoso, TableComponents } from 'react-virtuoso';
 
-export function BasicTable({ clients }: { clients: IClient[] }) {
-	return (
-		<TableContainer component={Paper} sx={{ maxWidth: '100%' }}>
-			<Table sx={{ minWidth: 400 }} aria-label='simple table'>
-				<TableHead>
-					<TableRow>
-						<TableCell>Name</TableCell>
-						<TableCell>Phone number</TableCell>
-						<TableCell>Email</TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{clients.map((client) => (
-						<ClientRow key={client.id} client={client} />
-					))}
-					{!clients ||
-						(!clients.length && (
-							<TableRow sx={{ padding: 3 }}>
-								<TableCell component='th' scope='row'>
-									No clients
-								</TableCell>
-							</TableRow>
-						))}
-				</TableBody>
-			</Table>
-		</TableContainer>
-	);
-}
-
 export default function ClientTable({ clients }: { clients: IClient[] }) {
 	return (
-		<Paper style={{ height: 400, width: '100%' }}>
+		<Paper style={{ height: 450, width: '100%' }}>
 			<TableVirtuoso
 				data={clients}
 				components={VirtuosoTableComponents}
@@ -56,8 +25,7 @@ function itemContent(_index: number, row: IClient) {
 	return (
 		<Fragment>
 			{columns.map(({ dataKey }) => {
-				// @ts-expect-error
-				const value = row[dataKey];
+				const value = row[dataKey as keyof IClient];
 				if (dataKey === 'name') {
 					return (
 						<TableCell key={dataKey} component='th' scope='row'>
@@ -91,12 +59,21 @@ function fixedHeaderContent() {
 
 const VirtuosoTableComponents: TableComponents<IClient> = {
 	Scroller: forwardRef<HTMLDivElement>((props, ref) => <TableContainer component={Paper} {...props} ref={ref} />),
-	Table: (props: any) => (
-		<Table {...props} sx={{ borderCollapse: 'separate', tableLayout: 'fixed' }} aria-label='simple table' />
+	Table: (props) => (
+		<Table {...props} sx={{ borderCollapse: 'separate', tableLayout: 'fixed' }} aria-label='Client Table' />
 	),
 	TableHead,
 	TableRow,
 	TableBody: forwardRef<HTMLTableSectionElement>((props, ref) => <TableBody {...props} ref={ref} />),
+	EmptyPlaceholder: function EmptyPlaceholder() {
+		return (
+			<TableRow>
+				<TableCell component='th' scope='row' colSpan={3}>
+					No clients
+				</TableCell>
+			</TableRow>
+		);
+	},
 };
 
 const columns = [
