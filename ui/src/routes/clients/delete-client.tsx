ed4826ca32c4b1564fad430a34acb5f1e75b1fa9
Dennis Cual <denniscual0716@gmail.com>
2023-08-05 03:06:07 +0800

    remove delete confirmation

--- ui/src/routes/clients/delete-client.tsx
+++ ui/src/routes/clients/delete-client.tsx
@@ -5,15 +5,7 @@ import { Form } from 'react-router-dom';
 
 export default function DeleteClient({ clientId }: { clientId: Client['id'] }) {
 	return (
-		<Form
-			method='delete'
-			action={`${clientId}/destroy`}
-			onSubmit={(event) => {
-				if (!window.confirm('Please confirm you want to delete this record.')) {
-					event.preventDefault();
-				}
-			}}
-		>
+		<Form method='delete' action={`${clientId}/destroy`}>
 			<input hidden type='text' name='clientId' defaultValue={clientId} />
 			<IconButton aria-label='delete' size='small' color='error' type='submit'>
 				<DeleteIcon fontSize='inherit' />
